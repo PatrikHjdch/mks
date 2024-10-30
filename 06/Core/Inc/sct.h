691c698eba1bf2b4a4c4c7e55b4718327b29f19b
PatrikHjdch <hajduch.patrik20@gmail.com>
2024-10-30 15:01:15 +0100

    cv6

---
+++ 06/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2024
+ *      Author: 240156
+ */
+#include "main.h"
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#endif /* INC_SCT_H_ */
+
+void sct_init();
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t led);
